406f0874e1e11f6c5c33ad2f02196ac396159250
rajat_1401 <rajat16260@iiitd.ac.in>
2024-02-25 19:48:29 +0530

    city POST working with autoinc

--- catalog/src/main/scala/com/bansal/washcatalog/catalog/domain/City.scala
+++ catalog/src/main/scala/com/bansal/washcatalog/catalog/domain/City.scala
@@ -25,6 +25,7 @@ case class CityInput(name: String, lang_code: String) extends Input
 
 case class City(
    id: String,
+   city_id: Option[Int]= None,
    name: String,
    lang_code: String,
    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ", timezone= "EET") created_on: Timestamp = new Timestamp(System.currentTimeMillis()),
@@ -35,14 +36,22 @@ case class CityOutput(id: String, name: String, language: Language, created_on: 
 object City extends RestEntity[City] with Logging {
   val table: TableQuery[CityTable]= TableQuery[CityTable]
 
+  def tupleToCity(id: Option[Int], name: String, lang_code: String, created_on: Timestamp, modified_on: Timestamp): City = {
+    City(id.map(_.toString).getOrElse("0"), id, name, lang_code, created_on, modified_on)
+  }
+
+  def cityToTuple(city: City): Option[(Option[Int], String, String, Timestamp, Timestamp)] = {
+    Some(city.city_id, city.name, city.lang_code, city.created_on, city.modified_on)
+  }
+
   def inputToTable(input: CityInput): City= {
-    City(id= "0", name= input.name, lang_code= input.lang_code)
+    City(id= "0", None, name= input.name, lang_code= input.lang_code)
   }
 
   def processQuery(id: Option[String]= None)(implicit ex: ExecutionContext): Future[PWResp]  = {
     val query= (for {
       cty <- table.filterOpt(id){
-        case (tbl, idd) => tbl.id === idd.toInt
+        case (tbl, idd) => tbl.city_id === idd.toInt
       }
       lng <- Language.table.filter(_.id === cty.lang_code)
     } yield (cty, lng)).result
@@ -55,7 +64,7 @@ object City extends RestEntity[City] with Logging {
     }
   }
 
-  override protected def getByIdQuery(id: String): FixedSqlStreamingAction[Seq[City], City, Effect.Read] = table.filter(_.id === id.toInt).result
+  override protected def getByIdQuery(id: String): FixedSqlStreamingAction[Seq[City], City, Effect.Read] = table.filter(_.city_id === id.toInt).result
 
   override protected def getAllQuery: FixedSqlStreamingAction[Seq[City], City, Effect.Read] = table.result
 
@@ -64,30 +73,25 @@ object City extends RestEntity[City] with Logging {
         queryMap,
         table,
         Entities.caseclassToMap[City]).asInstanceOf[Query[CityTable, City, Seq]]
-      .sortBy(_.id.asc.nullsLast).result
+      .sortBy(_.city_id.asc.nullsLast).result
   }
 
-  def addQuery(input: Input)(implicit ex: ExecutionContext): FixedSqlAction[Int, NoStream, Effect.Write] = {
+  def addQuery(input: Input)(implicit ex: ExecutionContext): FixedSqlAction[Any, NoStream, Effect.Write] = {
     val ctyInput = input.asInstanceOf[CityInput]
-    table += inputToTable(ctyInput)
-//    val insertQuery = table returning table.map(_.id) into((obj, id) => obj.copy(id = id.toString))
-//    insertQuery += inputToTable(ctyInput)
+    val insertQuery = table returning table.map(_.city_id) into((obj, id) => obj.copy(id = id.toString))
+    insertQuery += inputToTable(ctyInput)
   }
 
   def deleteQuery(id: String)(implicit ex: ExecutionContext): Future[String] = {
-    db.run(table.filter(_.id === id.toInt).delete).flatMap(resp => Future.successful(resp.toString))
+    db.run(table.filter(_.city_id === id.toInt).delete).flatMap(resp => Future.successful(resp.toString))
   }
 
   override def add(input: JsValue)(implicit ex: ExecutionContext, ws: WSClient, actorSystem: ActorSystem, headers: Map[String, Seq[String]]): Future[PWResp] = {
     val query = addQuery(objectMapper.readValue(input.toString(), classOf[CityInput]))
-    runDbAction(query)(logger).flatMap {
-      case SUCCESS => getById("0", isSimple = false)
+    db.run(query).flatMap {
+      case cty: City => getById(cty.id, isSimple = false)
       case _ => Future(PWResp(success = false, error = Some("Error while adding city")))
     }
-//    db.run(query).flatMap {
-//      case cty: City => getById("0", isSimple = false)
-//      case _ => Future(PWResp(success = false, error = Some("Error while adding city")))
-//    }
   }
 
   override def getById(id: String, isSimple: Boolean)(implicit ex: ExecutionContext, ws: WSClient, actorSystem: ActorSystem, headers: Map[String, Seq[String]]): Future[PWResp] = {
